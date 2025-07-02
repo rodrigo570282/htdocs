c536432e7a4ee3482be1c82cd5f2867e53e7b28f
Rodrigo Santos <rodrigorrs.sp@gmail.com>
2025-07-01 21:05:06 -0400

    01.07
    01.07

--- webapp/index.php
+++ webapp/index.php
@@ -17,7 +17,7 @@ $imagens = $galeriaModel->buscarTodas();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>File Upload</title>
+    <title>adicionar imagens</title>
 
     <style>
         <?php require_once __DIR__ . '/app/view/assets/style.css'; ?>
--- webapp/usuarioGaleria.php
+++ webapp/usuarioGaleria.php
@@ -21,7 +21,7 @@ $imagens = $galeriaModel->buscarPorUsuarioId($id);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeria de <?= $usuario['nome'] ?></title>
+    <title>Galeria de usuarios <?= $usuario['nome'] ?></title>
 
     <style>
         <?php require_once __DIR__ . '/app/view/assets/style.css'; ?>
